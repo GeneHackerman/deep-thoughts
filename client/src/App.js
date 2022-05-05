f4f8cf80afbde0112783d6ff378d2520a8afe21c
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-05-05 09:56:58 -0400

    added friendlist and created additional queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -18,13 +24,43 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+       {/*content within Routes will change according to URL route*/}
+       {/*each route will be a new page upon click*/}
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route 
+            path="/signup"
+            element={<Signup />}
+          />
+          <Route 
+            path="/profile/:username?"
+            element={<Profile />}
+          />
+          <Route
+            path="/thought/:id"
+            element={<SingleThought />}
+          />
+
+          <Route 
+            path="*"
+            element={<NoMatch />}
+          />
+        </Routes>
       </div>
       <Footer />
     </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- server/schemas/index.js
+++ server/schemas/index.js
@@ -2,3 +2,6 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
+
+
+// fix proxy server localhost:3001/
