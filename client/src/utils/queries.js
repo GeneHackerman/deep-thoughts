b3132c82c0e2c622571053bf0505be4ff3356003
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-05-05 11:43:43 -0400

    added user authentication

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -51,3 +51,43 @@ export const QUERY_THOUGHTS = gql`
                     reactionCount
                 }
             }`;
+
+        export const QUERY_ME = gql`
+        {
+            me {
+                _id
+                username
+                email
+                friendCount
+                thoughts { 
+                    _id
+                    thoughtText
+                    createdAt
+                    reactionCount
+                    reactions {
+                        _id
+                        createdAt
+                        reactionBody
+                        username
+                    }
+                }
+                friends {
+                    _id
+                    username
+                }
+            }
+        }`;
+
+        export const QUERY_ME_BASIC = gql`
+        {
+            me {
+                _id
+                username
+                email
+                friendCount
+                friends{
+                    _id
+                    username
+                }
+            }
+        }`;
